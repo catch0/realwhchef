b7bc5d0f8d570b92f60f6271c956aa3facd7ea7f
Spence Peacock <spencerpeacock@gmail.com>
2017-11-03 13:25:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const config = require('./config');
+const twit = require('twit');
+const T = new twit(config);
+const m = require('./data/meals.js');
+const bf = m.breakfast;
+const animals = require('animals');
+const cronJob = require('cron').CronJob;
+const stream = T.stream('user')
+
+// this function will grab a random animal and a random breakfast item and make a meal. it will then tweet out that meal
+// function breakFast(){
+//   var test = animals();
+//   var breakfast = bf[Math.floor(Math.random()*bf.length)];
+//   var bfjuice = animals();
+//   var temp = m.drinkTemp[Math.floor(Math.random()*m.drinkTemp.length)];
+//   params = {
+//     status: 'today for breakfast @realdonaldtrump is having ' + test + '-dick ' + breakfast + ' with a tall glass of '+ temp + " "+ bfjuice + ' urine'
+//
+//   }
+//   T.post('statuses/update', params, function(err, data, response){
+//     if(err){
+//       console.log(err)
+//     }
+//     console.log("post successfull!")
+//   })
+// }
+
+stream.on('follow',followed)
+
+function followed(eventMsg){
+  console.log("the follow bot is starting")
+  var screenName = eventMsg.source.screen_name;
+  tweetIt({status: "@" + screenName + "thanks for the follow we need all the support to make sure donald keeps getting his daily intake of dicks" });
+}
+
+
+function tweetIt(params){
+      T.post('statuses/update', params, tweeted);
+      function tweeted(err, data, response){
+        if(err){
+          console.log("there was an error", err);
+        }
+        else{
+          console.log("it worked!!!")
+        }
+    }
+}
+
+
+// breakFast();
+T.post('statuses/update', {status: "hello world!"})
